1e97fb255ce461d8dad7515de101ffffe74193ac
Mateus Craveiro <mccraveiro@gmail.com>
2019-01-04 21:39:17 -0200

    Add top breeds

--- src/item.js
+++ src/item.js
@@ -1,20 +1,55 @@
 const { createElement } = require('./custom-framework')
 
+function renderBreed(breed) {
+  return createElement(
+    'tr',
+    {},
+    createElement(
+      'th',
+      {},
+      breed.name,
+    ),
+    createElement(
+      'td',
+      {},
+      createElement(
+        'span',
+        {
+          className: 'histogram',
+          style: `width: ${breed.histogram}%`,
+        },
+      ),
+    ),
+    createElement(
+      'td',
+      {},
+      breed.count.toString(),
+    ),
+  )
+}
+
 function Item(props = {}) {
   return createElement(
     'li',
     {
       className: 'item',
     },
-    createElement('span', { className: 'item-position' }, props.position.toString()),
-    createElement('h2', { className: 'item-name' }, props.name),
-    createElement('span', { className: 'item-count' }, `${props.count} dogs`),
-    createElement('span', { className: 'item-male' }, `Male ${props.gender.male}%`),
-    createElement('span', { className: 'item-female' }, `Female ${props.gender.female}%`),
-    createElement('span', { className: 'item-unknown' }, `Unknown ${props.gender.unknown}%`),
-    createElement('hr'),
-    createElement('h3', { className: 'item-top-breeds-title' }, 'Top breeds'),
-    ...props.breeds.map(breed => createElement('h4', { className: 'item-breed-name' }, breed.name)),
+    createElement(
+      'header',
+      {},
+      createElement('span', { className: 'item-position' }, props.position.toString()),
+      createElement('h2', { className: 'item-name' }, props.name),
+      createElement('span', { className: 'item-count' }, `${props.count} dogs`),
+      createElement('span', { className: 'item-male' }, `Male ${props.gender.male}%`),
+      createElement('span', { className: 'item-female' }, `Female ${props.gender.female}%`),
+      createElement('span', { className: 'item-unknown' }, `Unknown ${props.gender.unknown}%`),
+    ),
+    createElement(
+      'table',
+      {},
+      createElement('caption', {}, 'Top breeds'),
+      ...props.breeds.map(renderBreed),
+    ),
   )
 }
 
--- src/item.test.js
+++ src/item.test.js
@@ -1,6 +1,6 @@
 const Item = require('./item')
 
-const fakeDog = {
+const example = {
   position: 1,
   name: 'Fluffy',
   count: 7652,
@@ -33,99 +33,132 @@ const fakeDog = {
   ],
 }
 
+const item = Item(example)
+
 test('returns an object', () => {
-  const item = Item(fakeDog)
   expect(typeof item).toBe('object')
 })
 
 test('returns a list item', () => {
-  const item = Item(fakeDog)
   expect(item.type).toBe('li')
 })
 
 test('returns item class', () => {
-  const item = Item(fakeDog)
   expect(item.props.className).toBe('item')
 })
 
+test('returns item header', () => {
+  expect(item.props.children[0].type).toBe('header')
+})
+
 test('returns dog position', () => {
-  const item = Item(fakeDog)
-  expect(item.props.children[0].type).toBe('span')
-  expect(item.props.children[0].props.children[0]).toBe('1')
-  expect(item.props.children[0].props.className).toBe('item-position')
+  const header = item.props.children[0]
+  expect(header.props.children[0].type).toBe('span')
+  expect(header.props.children[0].props.children[0]).toBe('1')
+  expect(header.props.children[0].props.className).toBe('item-position')
 })
 
 test('returns dog name', () => {
-  const item = Item(fakeDog)
-  expect(item.props.children[1].type).toBe('h2')
-  expect(item.props.children[1].props.children[0]).toBe('Fluffy')
-  expect(item.props.children[1].props.className).toBe('item-name')
+  const header = item.props.children[0]
+  expect(header.props.children[1].type).toBe('h2')
+  expect(header.props.children[1].props.children[0]).toBe('Fluffy')
+  expect(header.props.children[1].props.className).toBe('item-name')
 })
 
 test('returns dog count', () => {
-  const item = Item(fakeDog)
-  expect(item.props.children[2].type).toBe('span')
-  expect(item.props.children[2].props.children[0]).toBe('7652 dogs')
-  expect(item.props.children[2].props.className).toBe('item-count')
+  const header = item.props.children[0]
+  expect(header.props.children[2].type).toBe('span')
+  expect(header.props.children[2].props.children[0]).toBe('7652 dogs')
+  expect(header.props.children[2].props.className).toBe('item-count')
 })
 
 test('returns male count', () => {
-  const item = Item(fakeDog)
-  expect(item.props.children[3].type).toBe('span')
-  expect(item.props.children[3].props.children[0]).toBe('Male 36%')
-  expect(item.props.children[3].props.className).toBe('item-male')
+  const header = item.props.children[0]
+  expect(header.props.children[3].type).toBe('span')
+  expect(header.props.children[3].props.children[0]).toBe('Male 36%')
+  expect(header.props.children[3].props.className).toBe('item-male')
 })
 
 test('returns female count', () => {
-  const item = Item(fakeDog)
-  expect(item.props.children[4].type).toBe('span')
-  expect(item.props.children[4].props.children[0]).toBe('Female 55%')
-  expect(item.props.children[4].props.className).toBe('item-female')
+  const header = item.props.children[0]
+  expect(header.props.children[4].type).toBe('span')
+  expect(header.props.children[4].props.children[0]).toBe('Female 55%')
+  expect(header.props.children[4].props.className).toBe('item-female')
 })
 
 test('returns unknown count', () => {
-  const item = Item(fakeDog)
-  expect(item.props.children[5].type).toBe('span')
-  expect(item.props.children[5].props.children[0]).toBe('Unknown 9%')
-  expect(item.props.children[5].props.className).toBe('item-unknown')
+  const header = item.props.children[0]
+  expect(header.props.children[5].type).toBe('span')
+  expect(header.props.children[5].props.children[0]).toBe('Unknown 9%')
+  expect(header.props.children[5].props.className).toBe('item-unknown')
 })
 
-test('returns hr', () => {
-  const item = Item(fakeDog)
-  expect(item.props.children[6].type).toBe('hr')
+test('returns top breeds table', () => {
+  expect(item.props.children[1].type).toBe('table')
 })
 
-test('returns top breeds title', () => {
-  const item = Item(fakeDog)
-  expect(item.props.children[7].type).toBe('h3')
-  expect(item.props.children[7].props.children[0]).toBe('Top breeds')
-  expect(item.props.children[7].props.className).toBe('item-top-breeds-title')
+test('returns top breeds caption', () => {
+  const table = item.props.children[1]
+  expect(table.props.children[0].type).toBe('caption')
+  expect(table.props.children[0].props.children[0]).toBe('Top breeds')
 })
 
 test('returns Golden Retriever breed', () => {
-  const item = Item(fakeDog)
-  expect(item.props.children[8].type).toBe('h4')
-  expect(item.props.children[8].props.children[0]).toBe('Golden Retriever')
-  expect(item.props.children[8].props.className).toBe('item-breed-name')
+  const table = item.props.children[1]
+  const firstRow = table.props.children[1]
+  const { children } = firstRow.props
+  expect(firstRow.type).toBe('tr')
+  expect(children[0].type).toBe('th')
+  expect(children[0].props.children[0]).toBe('Golden Retriever')
+  expect(children[1].type).toBe('td')
+  expect(children[1].props.children[0].type).toBe('span')
+  expect(children[1].props.children[0].props.className).toBe('histogram')
+  expect(children[1].props.children[0].props.style).toBe('width: 100%')
+  expect(children[2].type).toBe('td')
+  expect(children[2].props.children[0]).toBe('3432')
 })
 
 test('returns Unknown breed', () => {
-  const item = Item(fakeDog)
-  expect(item.props.children[9].type).toBe('h4')
-  expect(item.props.children[9].props.children[0]).toBe('Unknown')
-  expect(item.props.children[9].props.className).toBe('item-breed-name')
+  const table = item.props.children[1]
+  const secondRow = table.props.children[2]
+  const { children } = secondRow.props
+  expect(secondRow.type).toBe('tr')
+  expect(children[0].type).toBe('th')
+  expect(children[0].props.children[0]).toBe('Unknown')
+  expect(children[1].type).toBe('td')
+  expect(children[1].props.children[0].type).toBe('span')
+  expect(children[1].props.children[0].props.className).toBe('histogram')
+  expect(children[1].props.children[0].props.style).toBe('width: 66%')
+  expect(children[2].type).toBe('td')
+  expect(children[2].props.children[0]).toBe('2298')
 })
 
 test('returns Pug breed', () => {
-  const item = Item(fakeDog)
-  expect(item.props.children[10].type).toBe('h4')
-  expect(item.props.children[10].props.children[0]).toBe('Pug')
-  expect(item.props.children[10].props.className).toBe('item-breed-name')
+  const table = item.props.children[1]
+  const thirdRow = table.props.children[3]
+  const { children } = thirdRow.props
+  expect(thirdRow.type).toBe('tr')
+  expect(children[0].type).toBe('th')
+  expect(children[0].props.children[0]).toBe('Pug')
+  expect(children[1].type).toBe('td')
+  expect(children[1].props.children[0].type).toBe('span')
+  expect(children[1].props.children[0].props.className).toBe('histogram')
+  expect(children[1].props.children[0].props.style).toBe('width: 33%')
+  expect(children[2].type).toBe('td')
+  expect(children[2].props.children[0]).toBe('1103')
 })
 
 test('returns Cocker Spaniel breed', () => {
-  const item = Item(fakeDog)
-  expect(item.props.children[11].type).toBe('h4')
-  expect(item.props.children[11].props.children[0]).toBe('Cocker Spaniel')
-  expect(item.props.children[11].props.className).toBe('item-breed-name')
+  const table = item.props.children[1]
+  const fourthRow = table.props.children[4]
+  const { children } = fourthRow.props
+  expect(fourthRow.type).toBe('tr')
+  expect(children[0].type).toBe('th')
+  expect(children[0].props.children[0]).toBe('Cocker Spaniel')
+  expect(children[1].type).toBe('td')
+  expect(children[1].props.children[0].type).toBe('span')
+  expect(children[1].props.children[0].props.className).toBe('histogram')
+  expect(children[1].props.children[0].props.style).toBe('width: 25%')
+  expect(children[2].type).toBe('td')
+  expect(children[2].props.children[0]).toBe('819')
 })
