20b8e77e5645d09cc3d07beccfd1668c563da3ec
Mateus Craveiro <mccraveiro@gmail.com>
2019-01-05 13:55:24 -0200

    Add build step for production

---
+++ webpack.prod.js
@@ -0,0 +1,17 @@
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      'src/index.html',
+      'src/index.css',
+    ])
+  ]
+}
